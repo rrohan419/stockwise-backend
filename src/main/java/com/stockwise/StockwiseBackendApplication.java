118b94bbfdb4de9de8702da2e580563efb09ea2c
Rohan <rohan.shrivastava@mindbowser.com>
2024-12-07 20:50:24 +0530

    initial commit

---
+++ src/main/java/com/stockwise/StockwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockwiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockwiseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockwise/StockwiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
